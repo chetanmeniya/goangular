3b3b35894bfa0b4f745bb258bc0588359dbe7833
rcmeniya <meniyachetan112@gmail.com>
2021-06-13 14:08:42 +0530

    first commit

--- src/Ccompany/backend/src/app/app.module.ts
+++ src/Ccompany/backend/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CompanyComponent } from './company/company.component';
+import { AddcompanyComponent } from './company/addcompany/addcompany.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CompanyComponent,
+    AddcompanyComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    DataTablesModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+        {path:'',component: DashboardComponent},
+        {path:'company/list',component: CompanyComponent}
+        {path:'company/insert',component: AddcompanyComponent}
+        {path:'company/update',component: AddcompanyComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
